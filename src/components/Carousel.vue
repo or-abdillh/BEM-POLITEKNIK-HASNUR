eb98524c2f82378a217ee0518b8b91f9a168b6e7
or-abdillh <hans.abdillh05@gmail.com>
2021-10-20 10:55:15 +0800

    Update caraousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,27 @@
 <template>
    <!-- splidejs -->
-   <section>
+   <section class="px-1">
       <Splide class="mt-40" :options="options">
          <SplideSlide>
-            <div class="w-6/12 ml-24 mt-40">
+            <div class="w-6/12 ml-24 my-40">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
-            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png" alt="Sample 1">
+            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png">
          </SplideSlide>
          <SplideSlide>
-            <img src="/hero.png" alt="Sample 2">
+            <div class="w-6/12 ml-24 mt-40">
+               <h1 class="text-5xl text-chatams text-color-1 font-bold">Merchandise juga tersedia disini</h1>
+               <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png">
+         </SplideSlide>
+         <SplideSlide>
+            <div class="w-6/12 ml-24 mt-40">
+               <h1 class="text-5xl text-chatams text-color-1 font-bold">Kamu mau curhat?</h1>
+               <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png" alt="Sample 1">
          </SplideSlide>
       </Splide>
    </section>
