12220847f3387aa728f37d1bc0882ce07c51d818
or-abdillh <hans.abdillh05@gmail.com>
2021-10-20 11:46:10 +0800

    Set image for each caraousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -7,21 +7,21 @@
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
-            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png">
+            <img class="absolute bottom-0 right-0 w-6/12" src="/caraousel-1.png" alt="caraousel-1">
          </SplideSlide>
          <SplideSlide>
             <div class="w-6/12 ml-24 mt-40">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Merchandise juga tersedia disini</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
-            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png">
+            <img class="absolute bottom-0 right-0 w-6/12" src="/caraousel-3.png">
          </SplideSlide>
          <SplideSlide>
             <div class="w-6/12 ml-24 mt-40">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Kamu mau curhat?</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
-            <img class="absolute bottom-0 right-16 w-4/12" src="/hero.png" alt="Sample 1">
+            <img class="absolute bottom-0 right-0 w-7/12" src="/caraousel-2.png" alt="Sample 1">
          </SplideSlide>
       </Splide>
    </section>
