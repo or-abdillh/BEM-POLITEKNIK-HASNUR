2862fa56ef9178c42e8c909d4a278fd9795e20f9
or-abdillh <hans.abdillh05@gmail.com>
2021-10-21 16:26:03 +0800

    Update caraousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,28 +2,33 @@
    img {
       z-index: -99;
    }
+   
+   .slide {
+      @apply w-6/12 top-2/4 absolute left-28;
+      transform: translateY(-50%);
+   }
 </style>
 
 <template>
    <!-- splidejs -->
    <section class="px-1">
-      <Splide class="mt-40" :options="options">
-         <SplideSlide>
-            <div class="w-6/12 ml-24 my-40">
+      <Splide class="h-screen" :options="options">
+         <SplideSlide class="h-screen h-40 relative">
+            <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <img class="absolute lg:-bottom-10 right-0 bottom-0 w-7/12 lg:w-6/12 xl:w-5/12" src="/caraousel-1.png" alt="caraousel-1">
          </SplideSlide>
          <SplideSlide>
-            <div class="w-6/12 ml-24 mt-40">
+            <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Merchandise juga tersedia disini</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <img class="absolute bottom-0 right-0 w-6/12" src="/caraousel-2.png">
          </SplideSlide>
          <SplideSlide>
-            <div class="w-6/12 ml-24 mt-40">
+            <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Kamu mau curhat?</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
