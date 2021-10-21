14ffd261b304ed3f48266f06f7fb0c90e3379986
or-abdillh <hans.abdillh05@gmail.com>
2021-10-21 16:36:57 +0800

    Update caraousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,31 +3,35 @@
       z-index: -99;
    }
    
+   .slide-wrapper {
+      height: 45vh;
+   }
+   
    .slide {
-      @apply w-6/12 top-2/4 absolute left-28;
+      @apply w-6/12 top-2/4 absolute left-20;
       transform: translateY(-50%);
    }
 </style>
 
 <template>
    <!-- splidejs -->
-   <section class="px-1">
-      <Splide class="h-screen" :options="options">
-         <SplideSlide class="h-screen h-40 relative">
+   <section class="px-1 mt-28">
+      <Splide :options="options">
+         <SplideSlide class="slide-wrapper lg:h-screen relative">
             <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <img class="absolute lg:-bottom-10 right-0 bottom-0 w-7/12 lg:w-6/12 xl:w-5/12" src="/caraousel-1.png" alt="caraousel-1">
          </SplideSlide>
-         <SplideSlide>
+         <SplideSlide class="slide-wrapper">
             <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Merchandise juga tersedia disini</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <img class="absolute bottom-0 right-0 w-6/12" src="/caraousel-2.png">
          </SplideSlide>
-         <SplideSlide>
+         <SplideSlide class="slide-wrapper">
             <div class="slide">
                <h1 class="text-5xl text-chatams text-color-1 font-bold">Kamu mau curhat?</h1>
                <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
