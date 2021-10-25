447c85f9eacba261511b1f903f13bcc16e28a85f
or-abdillh <hans.abdillh05@gmail.com>
2021-10-25 12:10:18 +0800

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,54 @@
 <template>
-    <section id="navbar">
-        <div class="navbar-wrapper">
-            <div class="flex gap-4 w-5/12 xl:w-3/12">
-                <img src="/logo.png">
-                <span>
-                   <h1 class="text-chatams font-bold">BEM POLITEKNIK HASNUR</h1>
-                   <p class="text-sunset text-sm">Kabinet Sinergi Koloborasi</p>
-                </span>
-            </div>
-            <!-- Nav link -->
-            <div class="ml-10">
-                <ul class="flex xl:gap-16 md:gap-5 w-full justify-between items-center">
-                  <li class="link" @click="dropDown = false">
-                     <router-link :to="{ name: 'home' }">Beranda</router-link> 
-                  </li>
-                  <!-- dropdown -->
-                  <li class="link relative" :class="dropDown ? 'bg-chatams' : ''">
-                     <small :class="dropDown ? 'text-gray-50' : ''" @click="dropDown = !dropDown" class="flex items-center gap-2">
-                        Tentang
-                        <i class="fa duration-300" :class="dropDown ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
-                     </small>
-                     <ul v-if="dropDown" class="dropdown bg-chatams">
-                        <li class="py-0 mb-3">
-                           <router-link to="">BEM Polihasnur</router-link>
-                        </li>
-                        <li class="py-0 mb-2">
-                           <router-link to="">Struktur Anggota</router-link>
-                        </li>
-                     </ul>
-                  </li> 
-                  <li class="link" @click="dropDown = false">
-                     <router-link :to="{ name: 'merchants' }">Merch</router-link>
-                  </li> 
-                  <li class="link" @click="dropDown = false">
-                     <router-link :class="isCurrentRoute ? 'router-link-active' : ''"  :to="{ name: 'info' }">BEM INFO</router-link>
-                  </li> 
-                  <li class="link" @click="dropDown = false">
-                     <router-link :to="{ name: 'contactUs' }">Hubungi Kami</router-link>
-                  </li> 
-                </ul>
-            </div>
+    <section class="navbar">
+       <!-- Brand -->
+        <div class="navbar-brand">
+           <img src="/logo.png" alt="">
+           <div>
+              <h1 class="text-chatams text-xl font-semibold">BEM POLITEKNIK HASNUR</h1>
+              <p class="text-sunset font-medium">Kabinet Sinergi Koloborasi</p>
+           </div>
         </div>
+        
+        <!-- Nav -->
+        <ul class="w-7/12 flex justify-between">
+           <li class="link">
+              <router-link :to="{ name: 'home' }">Beranda</router-link>
+           </li>
+           <li class="link relative" :class="dropDown ? 'bg-chatams' : ''">
+              <small :class="dropDown ? 'text-gray-50' : ''" @click="dropDown = !dropDown" class="flex items-center gap-2">
+                  Tentang
+                  <i class="fa duration-300" :class="dropDown ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
+               </small>
+               <ul v-if="dropDown" class="dropdown bg-chatams">
+                  <li class="py-0 mb-3">
+                     <router-link to="">BEM Polihasnur</router-link>
+                  </li>
+                  <li class="py-0 mb-2">
+                     <router-link to="">Struktur Anggota</router-link>
+                  </li>
+               </ul>
+           </li>
+           <li class="link">
+             <router-link :to="{ name: 'merchants' }">Merch</router-link>
+           </li>
+           <li class="link">
+             <router-link :class="isCurrentRoute ? 'router-link-active' : ''"  :to="{ name: 'info' }">BEM INFO</router-link>
+           </li>
+           <li class="link">
+             <router-link :to="{ name: 'contactUs' }">Hubungi Kami</router-link>
+           </li>
+        </ul>
     </section>
 </template>
 
 <style scoped>
    
-   #navbar .navbar-wrapper {
-     @apply z-10 fixed flex bg-white top-0 left-0 right-0 p-10;
+   .navbar {
+      @apply z-50 bg-white w-full px-10 fixed top-0 left-0 right-0 flex gap-20;
+   }
+   
+   .navbar-brand {
+      @apply flex gap-5 items-center py-10;
    }
    
    /* Nav Active */
@@ -57,7 +58,7 @@
    }
    
    .link {
-      @apply text-lg mb-6 px-4 rounded py-2;
+      @apply text-lg mb-6 px-4 rounded py-2 flex items-center;
       color: var(--chatams-blue);
    }
    
