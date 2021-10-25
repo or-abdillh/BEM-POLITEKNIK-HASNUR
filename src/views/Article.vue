bcfde2a230d46d9c6065756b2930c0eadcc94d20
or-abdillh <hans.abdillh05@gmail.com>
2021-10-25 09:06:49 +0800

    Create carousel in article view

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,23 +1,23 @@
 <template>
    <Carousel>
       <template v-slot:slide-1>
-         <div class="slide">
-            <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
-            <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+         <div class="slide-bottom">
+            <h1 class="text-3xl text-gray-50 font-bold">PKKMB politeknik hasnur 2021 akan dilaksanakan secara hybrid</h1>
+            <p class="text-xl font-medium text-gray-50 mt-4">Minggu, 12 September 2021</p>
          </div>
          <img src="/article-1.jpg" class="w-full" alt="" />
       </template>
       <template v-slot:slide-2>
-         <div class="slide">
-            <h1 class="text-5xl text-chatams text-color-1 font-bold">Selamat datang di Website <br> BEM POLIHASNUR!</h1>
-            <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+         <div class="slide-bottom">
+            <h1 class="text-3xl text-gray-50 font-bold">PKKMB politeknik hasnur dilaksanakan dengan lancar</h1>
+            <p class="text-xl font-medium text-gray-50 mt-4">Rabu, 15 September 2021</p>
          </div>
          <img src="/article-2.jpg" class="w-full" alt="" />
       </template>
       <template v-slot:slide-3>
-         <div class="slide">
-            <h1 class="text-5xl text-chatams text-color-1 font-bold">Kamu mau curhat?</h1>
-            <p class="text-3xl text-sunset mt-4 text-color-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+         <div class="slide-bottom">
+            <h1 class="text-2xl text-gray-50 font-bold">okumenter PKKMB politkenik hasnur 2021</h1>
+            <p class="text-xl font-medium mt-4 text-gray-50">Senin, 20 September 2021</p>
          </div>
          <img src="/article-3.jpg" class="w-full" alt="" />
       </template>
