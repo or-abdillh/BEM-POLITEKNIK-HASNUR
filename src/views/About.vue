e2eee98ffb03c73631dad55c7dc83d7caf42f3e8
or-abdillh <hans.abdillh05@gmail.com>
2021-10-26 09:50:11 +0800

    Update About views

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,6 +33,29 @@
          </div>
       </template>   
    </section>
+   
+   <!-- Filosofi Logo -->
+   <Title>
+      <template v-slot:title>
+         <h1>Filosofi Logo</h1>
+      </template>
+   </Title>
+   <img src="/BEM.jpg" class="mx-auto w-5/12" alt="" />
+    <section class="px-16 mt-10 justify-between gap-2 flex flex-wrap">
+      <template v-for="(text, index) in artiLambang" :key="index">
+         <div class="w-3/12 mb-16">
+            <div class="text-chatams mb-5 flex gap-1 items-end">
+               <h1 class="text-4xl font-semibold underline-cstm">0{{ index + 1 }}</h1>
+               <i class="fa fa-circle text-xs"></i>
+            </div>
+            <p class="text-2xl text-chatams font-medium">
+               {{ text }}
+            </p>
+         </div>
+      </template> 
+      <div class="w-3/12"></div>
+   </section>
+   <Footer></Footer>
 </template>
 
 <style scoped>
@@ -49,15 +72,24 @@
    import Cover from '../components/Cover.vue'
    import About from '../components/About.vue'
    import Title from '../components/Title.vue'
+   import Footer from '../components/Footer.vue'
    
    import { ref } from 'vue'
    
    const misi = ref([
-    "Menyelenggarakan keorganisasian yang berpacu pada keimanan dan ketakwaan terhadap Tuhan YME.",
-    "Meningkatkan kinerja anggota BEM Polihasnur  dengan membangun komunikasi dan manajemen yang baik dalam ruang lingkup kekeluargaan.",
-    "Menjunjung tinggi almamater dalam berbagai kehidupan kemahasiswaan dan kehidupan bermasyarakat.",
-    "Meningkatkan mutu serta pengelolaan Unit Kegiatan Mahasiswa dan organisasi-organisasi yang ada di Politeknik Hasnur untuk mengembangkan Soft Skill mahasiswa.",
-    "Mengemban aspirasi mahasiswa Politeknik Hasnur dalam menciptakan kondisi kampus yang demokratis.",
-    "Menampung dan menyalurkan aspirasi serta kepedulian mahasiswa terhadap persoalan -persoalan sosial kemasyarakatan."
-    ])
+      "Menyelenggarakan keorganisasian yang berpacu pada keimanan dan ketakwaan terhadap Tuhan YME.",
+      "Meningkatkan kinerja anggota BEM Polihasnur  dengan membangun komunikasi dan manajemen yang baik dalam ruang lingkup kekeluargaan.",
+      "Menjunjung tinggi almamater dalam berbagai kehidupan kemahasiswaan dan kehidupan bermasyarakat.",
+      "Meningkatkan mutu serta pengelolaan Unit Kegiatan Mahasiswa dan organisasi-organisasi yang ada di Politeknik Hasnur untuk mengembangkan Soft Skill mahasiswa.",
+      "Mengemban aspirasi mahasiswa Politeknik Hasnur dalam menciptakan kondisi kampus yang demokratis.",
+      "Menampung dan menyalurkan aspirasi serta kepedulian mahasiswa terhadap persoalan -persoalan sosial kemasyarakatan."
+   ])
+    
+   const artiLambang = ref([
+      "Padi dan Kapas melambangkan kesejahteraan dan kenyamanan, diharapkan BEM Polihasnur dapat memberikan kesejahteraan dan keamanan serta menjadi wadah bagi seluruh aspirasi para mahasiswa Politeknik Hasnur.",
+      "Bentuk perisai pada logo melambangkan BEM Polihasnur merupakan sebuah organisasi yang kuat dan dapat bertahan dalam kondisi sesulit apapun.",
+      "Warna abu-abu pada kata BEM mempunyai arti profesionalitas, kepandaian, dan keseriusan.",
+      "Warna kuning mempunyai arti energi sosial, kerjasama dan loyalitas.",
+      "Bola dunia melambangkan Perguruan Tinggi Politeknik Hasnur yang dalam beberapa tahun ke depan akan menjadi pusat pendidikan terapan yang unggul di tingkat nasional dan internasional."
+   ])
 </script>
